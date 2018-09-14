e3b9d4e4eca724c6ce5d058159d8aae8f5f8948b
mariano <mariano1colombo@hotmail.com>
2018-09-14 15:05:15 -0300

    Agregado template de login t bootstrap

--- config/routes.js
+++ config/routes.js
@@ -27,6 +27,9 @@ module.exports.routes = {
   '/': {
     view: 'pages/homepage'
   },
+  '/login': {
+    view: 'pages/login'
+  },
 
   /***************************************************************************
   *                                                                          *
