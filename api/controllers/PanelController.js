1507b36b6b11fbb41b8bb1ea2e24881d21c36a3f
mariano <mariano1colombo@hotmail.com>
2018-09-18 00:05:09 -0300

    Check de Login y permisos

--- api/controllers/PanelController.js
+++ api/controllers/PanelController.js
@@ -6,12 +6,57 @@
  */
 
 module.exports = {
-  showPanel: function (req,res) {
+  showPanel: async function (req,res) {
+
+    const menu = [
+      {
+        title: "HOME",
+        link: "/panel/home",
+        icon: "nc-chart-pie-35",
+        permissions: ["admin"],
+        name: "home",
+      },
+      {
+        title: "User Profile",
+        link: "panel/profile",
+        icon: "nc-circle-09",
+        permissions: ["admin"],
+        name: "profile",
+      }
+      ];
+
+
+    LoginService.checkLogin(req,res);
+
     let seccion = req.param("section");
     if (seccion==null) {
       res.redirect("/panel/home");
     }
-    res.view("pages/"+seccion, {layout: 'layouts/admin'});
+    let user = await User.find({id: req.session.userId});
+    let accessGranted = false;
+
+    _.each(menu, function (item) {
+      if (item.name == seccion) {
+        if (item.permissions.includes(user[0].accessLvl)) {
+          accessGranted = true;
+        }
+      }
+    });
+
+    sails.log(accessGranted);
+
+    if (!accessGranted) {
+      res.notFound();
+    }
+    else {
+      res.view("pages/"+seccion, {
+        layout: 'layouts/admin',
+        user: user[0],
+        menu: menu,
+        path: req.path,
+      });
+    }
+
   }
 
 };
--- api/services/LoginService.js
+++ api/services/LoginService.js
@@ -2,9 +2,9 @@ module.exports = {
   checkLogin: function (req,res) {
 
     if (req.path!="/login" && req.session.userId == null) {
-      sails.log(req.path);
         res.redirect("/login");
+        return false;
       }
-    return;
+    return true;
   }
 };
