5ff8faf16617282ad7ab3d43d6761bd987c8c72b
mariano <mariano1colombo@hotmail.com>
2018-09-20 22:58:44 -0300

    comentado el codigo

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -9,9 +9,11 @@ module.exports = {
 
 
   show: function (req, res) {
+    //Envio la view home usando el layout de admin
     res.view("pages/home", {layout: "layouts/admin"});
   },
   redirect: function (req, res) {
+    //Redirecciono a /panel/login
     res.redirect("/panel/home");
   },
 
