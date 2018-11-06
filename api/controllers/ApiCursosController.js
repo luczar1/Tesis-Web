38201ec014021934ea02978a0949c53dcb84f23f
mariano <mariano1colombo@hotmail.com>
2018-11-06 14:40:48 -0300

    CAmbiado nombre del controller de api cursos

--- api/controllers/ApiCursosController.js
+++ api/controllers/ApiCursosController.js
@@ -1,20 +1,12 @@
 /**
- * ApiCursosControllerController
+ * ApiCursosController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
-  getListadoCursos: function (req, res) {
-
-
-
-
-
-  },
-
-
+  
 
 };
 
