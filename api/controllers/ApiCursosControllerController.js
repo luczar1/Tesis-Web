540dcd1b0ff647348d0c6ebb314482a4dca77bda
mariano <mariano1colombo@hotmail.com>
2018-11-06 14:36:00 -0300

    Generado ApiCursosController para hacer un get de los cursos y sus datos

---
+++ api/controllers/ApiCursosControllerController.js
@@ -0,0 +1,20 @@
+/**
+ * ApiCursosControllerController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  getListadoCursos: function (req, res) {
+
+
+
+
+
+  },
+
+
+
+};
+
