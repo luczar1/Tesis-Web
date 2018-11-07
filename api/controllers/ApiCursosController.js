038d6d59fd55646d5d31f624bfdb2187bdbc6f00
mariano <mariano1colombo@hotmail.com>
2018-11-06 23:11:29 -0300

    Agregada Api de para poder obtener los cursos desde la App

--- api/controllers/ApiCursosController.js
+++ api/controllers/ApiCursosController.js
@@ -6,7 +6,27 @@
  */
 
 module.exports = {
-  
+  getCursos: async function (req, res) {
+
+    let cursos = await Curso.find(
+      {
+        select: ['id', 'nombre', 'img'],
+        where: {
+          estado: {'!=': 'Terminado'}
+        }
+      });
+
+    res.json(cursos);
+
+  },
+  getCurso: async function (req, res) {
+
+
+    let curso = await Curso.findOne({id: req.param('id')});
+
+    res.json(curso);
+
+  },
 
 };
 
