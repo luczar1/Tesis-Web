29aa9a5e02b5275d1e4a8179ea85bc77ddc87be2
mariano <mariano1colombo@hotmail.com>
2018-12-10 20:52:27 -0300

    Limit de peticiones

--- config/blueprints.js
+++ config/blueprints.js
@@ -26,7 +26,22 @@ module.exports.blueprints = {
   *                                                                          *
   ***************************************************************************/
 
-  // rest: true,
+   rest: true,
+
+  parseBlueprintOptions: function(req) {
+
+    // Get the default query options.
+    var queryOptions = req._sails.hooks.blueprints.parseBlueprintOptions(req);
+
+    // If this is the "find" or "populate" blueprint action, and the normal query options
+    // indicate that the request is attempting to set an exceedingly high `limit` clause,
+    // then prevent it (we'll say `limit` must not exceed 100).
+
+        queryOptions.criteria.limit = 1000000;
+
+    return queryOptions;
+
+  }
 
 
   /***************************************************************************
