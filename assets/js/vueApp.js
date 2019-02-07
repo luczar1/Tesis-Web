dde70d596c39fcc772d48d9a1d6769cd4a722e7d
mariano <mariano1colombo@hotmail.com>
2019-02-07 14:19:54 -0300

    Modal envio de notif

--- assets/js/vueApp.js
+++ assets/js/vueApp.js
@@ -299,9 +299,9 @@ Vue.component('box-curso', {
         <div class="row pt-3">
           <div class="col-sm-12">
           <nav class="nav nav-pills">
-            <a class="nav-link" :class="{'active': alumnosSection == 'todos'}" href="#" @click="changeSectionAlumnos('todos')">Todos <span :class="{'badge badge-light': alumnosSection == 'todos','badge badge-primary': alumnosSection == 'todos', }">{{getListadoAlumnos('all').length}}</span></a>
-            <a class="nav-link" :class="{'active': alumnosSection == 'solicitudes'}" href="#" @click="changeSectionAlumnos('solicitudes')">Solicitudes ({{getListadoAlumnos('solicitudes').length}})</a>
-            <a class="nav-link" :class="{'active': alumnosSection == 'inscriptos'}" href="#" @click="changeSectionAlumnos('inscriptos')">Inscriptos ({{getListadoAlumnos('inscriptos').length}})</a>
+            <a class="nav-link" :class="{'active': alumnosSection == 'todos'}" href="#" @click="changeSectionAlumnos('todos')">Todos <span :class="{'badge badge-light': alumnosSection == 'todos','badge badge-primary': alumnosSection != 'todos', }">{{getListadoAlumnos('all').length}}</span></a>
+            <a class="nav-link" :class="{'active': alumnosSection == 'solicitudes'}" href="#" @click="changeSectionAlumnos('solicitudes')">Solicitudes <span :class="{'badge badge-light': alumnosSection == 'solicitudes','badge badge-primary': alumnosSection != 'solicitudes', }">{{getListadoAlumnos('solicitudes').length}}</span></a>
+            <a class="nav-link" :class="{'active': alumnosSection == 'inscriptos'}" href="#" @click="changeSectionAlumnos('inscriptos')">Inscriptos <span :class="{'badge badge-light': alumnosSection == 'inscriptos','badge badge-primary': alumnosSection != 'inscriptos', }">{{getListadoAlumnos('inscriptos').length}})</span></a>
           </nav>
           </div>
         </div>
