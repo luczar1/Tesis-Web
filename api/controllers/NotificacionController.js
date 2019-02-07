10ed620e763f1040b8606e277afbe8b45d41c256
mariano <mariano1colombo@hotmail.com>
2019-02-07 17:00:18 -0300

    Controller para enviar notificaciones

--- api/controllers/NotificacionController.js
+++ api/controllers/NotificacionController.js
@@ -9,10 +9,42 @@ module.exports = {
 
 
   create : async function(req, res) {
-    let notificacion = req.param("notificacion");
+    let titulo = req.param("titulo");
+    let mensaje = req.param("mensaje");
 
-    res.json(notificacion);
+    let alumnos = req.param('alumnos');
+    let docentes = req.param('docentes');
+
+
+    for (let alumno of alumnos) {
+      if (process.env.NODE_ENV == 'production') {
+
+      } else {
+        if (alumno.sendNotifApp) {
+
+        }
+        if (alumno.sendNotifEmail) {
+          await sails.helpers.sendEmail.with({to: 'lucas.zarza@gmail.com', subject: "Tienes un nuevo mensaje de la fundacion Jean Sonet", text: "titulo: " + titulo + "Mensaje: " + mensaje});
+        }
+      }
+    }
+    for (let docente of docentes) {
+      if (process.env.NODE_ENV == 'production') {
+
+      } else {
+        if (docente.sendNotifApp) {
+
+        }
+        if (docente.sendNotifEmail) {
+          await sails.helpers.sendEmail.with({to: 'lucas.zarza@gmail.com', subject: "Tienes un nuevo mensaje de la fundacion Jean Sonet", text: "titulo: " + titulo + "Mensaje: " + mensaje});
+        }
+      }
+    }
+
+    res.ok();
   }
+
+
 
 };
 
