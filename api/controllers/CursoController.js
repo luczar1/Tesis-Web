926369d7c24863649a9cd0ec8f72944b9cb13411
SantiagoMerlo <galaxymerlo@gmail.com>
2019-02-12 13:05:12 +0000

    Listado de cursos para profesor

--- api/controllers/CursoController.js
+++ api/controllers/CursoController.js
@@ -55,7 +55,7 @@ module.exports = {
   findOne: async function(req, res) {
 
     //Si es admin, devuelvo los datos completos y ademas busco los datos de la tabla intermedia de profesores y alumnos
-    if (await User.isAdmin(req.session)) {
+    if (await User.isAdmin(req.session) || await User.isDocente(req.session)) {
       let curso = await Curso.findOne({id: req.param('id')}).populate('alumnos').populate('docentes');
 
       for (let alumno of curso.alumnos) {
